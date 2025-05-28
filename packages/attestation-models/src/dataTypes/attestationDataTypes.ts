59822454a027a9c6dfa72ffa9aabb9622b925929
Martyn Janes <martyn@obany.com>
2025-05-28 14:19:21 +0200

    feat: update data type registration with fully qualified names

--- packages/attestation-models/src/dataTypes/attestationDataTypes.ts
+++ packages/attestation-models/src/dataTypes/attestationDataTypes.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0.
 import { DataTypeHandlerFactory } from "@twin.org/data-core";
 import type { JSONSchema7 } from "json-schema";
+import { AttestationContexts } from "../models/attestationContexts";
 import { AttestationTypes } from "../models/attestationTypes";
 import AttestationInformationSchema from "../schemas/AttestationInformation.json";
 import AttestationJwtProofSchema from "../schemas/AttestationJwtProof.json";
@@ -14,15 +15,23 @@ export class AttestationDataTypes {
 	 * Register all the data types.
 	 */
 	public static registerTypes(): void {
-		DataTypeHandlerFactory.register(AttestationTypes.Information, () => ({
-			type: AttestationTypes.Information,
-			defaultValue: {},
-			jsonSchema: async () => AttestationInformationSchema as JSONSchema7
-		}));
-		DataTypeHandlerFactory.register(AttestationTypes.JwtProof, () => ({
-			type: AttestationTypes.JwtProof,
-			defaultValue: {},
-			jsonSchema: async () => AttestationJwtProofSchema as JSONSchema7
-		}));
+		DataTypeHandlerFactory.register(
+			`${AttestationContexts.ContextRoot}${AttestationTypes.Information}`,
+			() => ({
+				context: AttestationContexts.ContextRoot,
+				type: AttestationTypes.Information,
+				defaultValue: {},
+				jsonSchema: async () => AttestationInformationSchema as JSONSchema7
+			})
+		);
+		DataTypeHandlerFactory.register(
+			`${AttestationContexts.ContextRoot}${AttestationTypes.JwtProof}`,
+			() => ({
+				context: AttestationContexts.ContextRoot,
+				type: AttestationTypes.JwtProof,
+				defaultValue: {},
+				jsonSchema: async () => AttestationJwtProofSchema as JSONSchema7
+			})
+		);
 	}
 }
